faa1260d773ad7502f83882820a7b149ba7325aa
Gentrit Biba <43895066+gentritbiba@users.noreply.github.com>
2021-05-03 19:45:52 +0200

    Update Html.js

--- src/components/Html.js
+++ src/components/Html.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { TextField, Button } from "@material-ui/core"
+import { TextField, Button, InputLabel, MenuItem, Select, FormControl } from "@material-ui/core"
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
-    textField:{
-        width:'100%'
+    textField: {
+        width: '100%'
     },
-    button:{
-        marginTop:30
-    }
+    button: {
+        marginTop: 30
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+      }
 }));
 
 
@@ -17,15 +21,15 @@ const useStyles = makeStyles((theme) => ({
 export function Html(props) {
     const classes = useStyles();
     const [text, setText] = useState("");
-    const {questions, setQuestions, updateJsonBody} = props;
-    
-    const generateHTML = (text)=>{
-        const questionRegex = "<h3>(.|\n)*?<\/h3>";    
+    const [qtype, setQtype] = useState("h2");
+    const { questions, setQuestions, updateJsonBody } = props;
+
+    const generateHTML = (text) => {
+        const questionRegex = "<h3>(.|\n)*?<\/h3>";
         const answerRegex = "<\/h3>(.|\n)*?<h3>";
         text = text.replace(/(\r\n|\n|\r)/gm, "");
-        console.log(text);
-        const questionsRes = [...text.matchAll(questionRegex)].map(el=>{return el[0].replace(/<\/?h3>/g,'');})
-        const answersRes = [...text.matchAll(answerRegex)].map(el=>{return el[0].replace(/<\/?h3>/g,'');})
+        const questionsRes = [...text.matchAll(questionRegex)].map(el => { return el[0].replace(/<\/?h3>/g, ''); })
+        const answersRes = [...text.matchAll(answerRegex)].map(el => { return el[0].replace(/<\/?h3>/g, ''); })
         let newQuestions = []
         questionsRes.forEach((el, indx) => {
             newQuestions.push({
@@ -46,9 +50,22 @@ export function Html(props) {
                 value={text}
                 className={classes.textField}
                 variant="filled"
-                onChange={(el)=>{setText(el.target.value)}}
+                onChange={(el) => { setText(el.target.value) }}
             />
-            <Button variant="contained" className={classes.button} color="primary" onClick={() => {generateHTML(text + "<h3>")}}>Generate</Button>
+            <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">Element Type</InputLabel>
+                <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={qtype}
+                    onChange={(el) => { setQtype(el.target.value) }}
+                    label="Question Element Type"
+                >
+                    <MenuItem value={"h2"}>{"<h2>"}</MenuItem>
+                    <MenuItem value={"p"}>{"<p>"}</MenuItem>
+                </Select>
+            </FormControl>
+            <Button variant="contained" className={classes.button} color="primary" onClick={() => { generateHTML(text + "<h3>") }}>Generate</Button>
 
         </div>
     )
